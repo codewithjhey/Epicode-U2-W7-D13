8291b40e0d34cac426d048d4252d980d8e930abb
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2022-11-10 08:02:36 +0000

    async func fixed

--- index.js
+++ index.js
@@ -1,42 +1,27 @@
 const getBooks = async () => {
-  let unreadableResult = await fetch(
-    "https://striveschool-api.herokuapp.com/books"
-  )
-  let result = await unreadableResult.json()
-  let data = result.data
-  console.log(data)
+  let response = await fetch("https://striveschool-api.herokuapp.com/books")
+  let books = await response.json()
+  console.log(books)
 
   let container = document.querySelector(".firstRow")
-  result.forEach((data) => {
-    let { book } = data
-    container.innerHTML += `<div class="col-md-4">
+  books.forEach((book) => {
+    container.innerHTML += `<div class="col-md-4 ">
   <div class="card mb-4 shadow-sm">
     <img src=${book.img} class="card-img-top" alt="...">
     
     <div class="card-body">
-      <p class="card-text">${book.title}
-      </p>
-      <div
-        class="d-flex justify-content-between align-items-center"
-      >/attachments/1024673103206432769/1039931950426755072/Screenshot_2022-11-09_at_3.58.03_PM.png
-        <div class="btn-group">
-          <button
-            type="button"
-            class="btn btn-sm btn-outline-secondary"
-          >
-            View
-          </button>
-          <button
-            type="button"
-            class="btn btn-sm btn-outline-secondary hideBtn"
-          >
-            Hide
-          </button>
-        </div>
-        <small class="text-muted">9 mins</small>
+    <h5 class="card-title">${book.title}</h5>
+      <p class="card-text">Category: ${book.category}</p>
+      <p class="card-text">Price: $ ${book.price}</p>
+      <div class="card-buttons">
+      <button class="btn btn-primary add-button">Add To Cart</button>
+      <button class="btn btn-danger skip-button">Skip</button>
+    </div>
       </div>
     </div>
   </div>
 </div>`
   })
 }
+
+window.onload = getBooks
